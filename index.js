0d8f07c240b12d21a3e1a3d6b0636ba8335c9f7d
Rahees Ahmed <raheesahmed256@gmail.com>
2023-12-31 02:34:57 +0500

    Update dependencies and add image generation endpoint

--- index.js
+++ index.js
@@ -1,75 +1,92 @@
+import express from "express";
 import puppeteer from "puppeteer";
 import OpenAI from "openai";
 import dotenv from "dotenv";
-import fs from "fs";
+import cheerio from "cheerio";
 
 dotenv.config();
 
-const apiKey = process.env.OPENAI_API_KEY;
-const openai = new OpenAI(apiKey);
+const app = express();
+app.use(express.json());
 
-async function generateImages(prompt) {
+const apiKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey });
+
+app.post("/generate-images", async (req, res) => {
+  const { url, colorCodes } = req.body;
+
+  try {
+    const htmlContent = await scrapeWebsite(url);
+    const summarizedContent = extractKeyContent(htmlContent);
+    const prompt = createPrompt(summarizedContent, colorCodes);
+    const images = await generateImages(prompt);
+    res.json({ images });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    res.status(500).send("Error generating images");
+  }
+});
+
+const scrapeWebsite = async (url) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle2" });
+  const content = await page.content();
+  await browser.close();
+  return content;
+};
+
+const extractKeyContent = (htmlContent) => {
+  const $ = cheerio.load(htmlContent);
+  let summarizedContent = "";
+
+  // Extract text from key elements like headings and paragraphs
+  $("h1, h2, h3, p").each((_, element) => {
+    summarizedContent += $(element).text() + " ";
+  });
+
+  return summarizedContent;
+};
+
+const createPrompt = (content, colorCodes) => {
+  const MAX_LENGTH = 400; // Adjust as needed
+  let truncatedContent =
+    content.length > MAX_LENGTH
+      ? content.substring(0, MAX_LENGTH) + "..."
+      : content;
+
+  let prompt = `Create an advertisement image based on the following content: ${truncatedContent}`;
+  if (colorCodes && colorCodes.length) {
+    prompt += `. Use the following colors: ${colorCodes.join(", ")}`;
+  }
+  return prompt;
+};
+
+const generateImages = async (prompt) => {
   try {
     const response = await openai.images.generate({
-      model: "gpt-4-vision-preview",
-      prompt: prompt,
+      model: "dall-e-3",
+      prompt,
+      n: 1, // As per the documentation, DALL-E 3 currently supports generating only one image per request
+      quality: "standard", // Optional: Adjust as needed
+      size: "1024x1024", // Optional: Adjust as needed
+      response_format: "url", // Specify the format of the response
     });
 
-    if (response && response.data && response.data.images) {
-      return response.data.images;
+    // Check if the response has the expected structure
+    if (response && response.data && Array.isArray(response.data)) {
+      return response.data.map((image) => image.url);
     } else {
-      throw new Error("Invalid response from DALL-E API");
+      console.error("Unexpected response structure:", response);
+      return []; // Return an empty array or handle as needed
     }
   } catch (error) {
     console.error("Error generating images:", error);
     throw error;
   }
-}
-
-const extractPageContent = async (page) => {
-  try {
-    return await page.evaluate(() => document.body.innerText);
-  } catch (error) {
-    console.error("Error extracting page content:", error);
-    throw error;
-  }
 };
 
-const createPrompt = (content) => {
-  try {
-    const analyticsContent = fs.readFileSync("IMGPT analytics.txt", "utf-8");
-    const promptContent = fs.readFileSync("IMGPT.txt", "utf-8");
-
-    let prompt = `${content}\n${analyticsContent}\n${promptContent}`;
-    if (content.includes("important keyword")) {
-      prompt += "\nAdditional prompt for important keyword";
-    } else {
-      prompt += "\nDefault prompt";
-    }
-
-    console.log("Generated Prompt:", prompt); // Logging the generated prompt
-    return prompt;
-  } catch (error) {
-    console.error("Error creating prompt:", error);
-    throw error;
-  }
-};
-
-(async () => {
-  try {
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-    await page.goto("https://teletimesinternational.com/", {
-      waitUntil: "networkidle2",
-    });
-
-    const content = await extractPageContent(page);
-    const prompt = createPrompt(content);
-    const images = await generateImages(prompt);
-
-    console.log("Generated Images:", images);
-    await browser.close();
-  } catch (error) {
-    console.error("Error:", error);
-  }
-})();
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
